b913296cc40bb7b0519731b812775b8d2c2db349
maple <zhujin951107@gmail.com>
2022-11-17 20:37:29 +0800

    chore: play with fastapi

---
+++ debug.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+print(ModelName.alexnet)
--- main.py
+++ main.py
@@ -1,7 +1,78 @@
+from pydantic import BaseModel
+from enum import Enum
+from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get('/')
 async def root():
     return {"msg": "helloworld"}
+
+
+# path parameter with variable type
+@app.get('/items/{item_id}')
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+# path with enum type
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+fake_items_db = [{"item_name": "Foo"}, {
+    "item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+# query
+@app.get("/fakeitems/")
+async def read_fakeitem(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
+
+@app.get("/fakeitems/{item_id}")
+async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+# request body
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+
+@app.post("/items/")
+async def create_item(item: Item):  # item is request body
+    return item.dict()
+
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item, q: Union[str, None] = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
